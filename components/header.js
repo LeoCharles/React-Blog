e0065f6d43dd7ceecb5ce0baa5a7515506ceb8a1
leofe <leofe200@163.com>
2019-11-28 01:10:06 +0800

    :sparkles: add header

--- components/header.js
+++ components/header.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import { Layout, Row, Col, Menu, Icon } from 'antd'
+import { Layout, Row, Col, Menu, Icon, Button, Input } from 'antd'
 import '../static/style/components/header.css'
 
-// const Header = Layout.Header
+const Header = Layout.Header
 
 const CommonHeader = () => {
   return (
-    <div className="header">
-      <Row type="flex" justify="center">
-        <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+    <Header className="header">
+      <Row>
+        <Col className="header-left" xs={24} sm={24} md={8} lg={6} xl={4}>
           <span className="header-logo">刘木木的博客</span>
         </Col>
-        <Col xs={0} sm={0} md={14} lg={8} xl={6}>
-          <Menu mode="horizontal">
+        <Col className="header-right" xs={0} sm={0} md={16} lg={18} xl={20}>
+          <div className="header-search">
+            <Icon type="search" className="search-icon"/>
+            <Input type="text" value={""} className="search-input" placeholder='搜索文章'/>
+          </div>
+          <Menu className="header-nav" mode="horizontal">
             <Menu.Item key="home">
               <Icon type="home" />首页
             </Menu.Item>
@@ -23,9 +27,13 @@ const CommonHeader = () => {
               <Icon type="user" />关于
             </Menu.Item>
           </Menu>
+          <div className="header-userinfo">
+            <Button>登录</Button>
+            <Button>注册</Button>
+          </div>
         </Col>
       </Row>
-    </div>
+    </Header>
   )
 }
 
