076fdbbf9e0bcc094ba462172f577c7eec571e5d
leofe <leofe200@163.com>
2019-11-28 23:13:36 +0800

    :sparkles:  modify

--- components/Sider/index.js
+++ components/Sider/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon } from 'antd'
+import { Icon, Divider, Tag} from 'antd'
 import SvgIcon from '../SvgIcon/index'
 import '../../static/style/sider.css'
 
@@ -18,6 +18,44 @@ const Sider = () => {
             <SvgIcon type='iconjuejin' className='link-icon' />掘金
           </span>
         </div>
+      </div>
+      <Divider orientation="left">热门文章</Divider>
+      <ul className="article-list">
+        <li className="article-item">
+          <a>koa-react-ssr 学习</a>
+        </li>
+        <li className="article-item">
+          <a>typescript 入门</a>
+        </li>
+        <li className="article-item">
+          <a>实现简易的 VUE-MVVM</a>
+        </li>
+        <li className="article-item">
+          <a>实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM</a>
+        </li>
+        <li className="article-item">
+          <a>如何用 es6+ 写出优雅的 js 代码</a>
+        </li>
+        <li className="article-item">
+          <a>实现简易的 VUE-MVVM</a>
+        </li>
+        <li className="article-item">
+          <a>实现简易的 VUE-MVVM</a>
+        </li>
+      </ul>
+      <Divider orientation="left">标签</Divider>
+      <div className="tag-list">
+        <Tag color={'geekblue'}><a>Vue</a></Tag>
+        <Tag color={'lime'}><a>ES6</a></Tag>
+        <Tag color={'volcano'}><a>Vue</a></Tag>
+        <Tag color={'green'}><a>Koa</a></Tag>
+        <Tag color={'cyan'}><a>NodeJs</a></Tag>
+        <Tag color={'gold'}><a>React</a></Tag>
+        <Tag color={'geekblue'}><a>TypeScript</a></Tag>
+        <Tag color={'volcano'}><a>MySql</a></Tag>
+        <Tag color={'purple'}><a>NodeJs</a></Tag>
+        <Tag color={'geekblue'}><a>Javascript</a></Tag>
+        <Tag color={'magenta'}><a>React</a></Tag>
       </div>
     </div>
   )
---
+++ utils/config.js
@@ -0,0 +1,15 @@
+// 常用配置
+
+// 颜色列表
+export const COLOR_LIST = [
+  'geekblue',
+  'magenta',
+  'blue',
+  'volcano',
+  'orange',
+  'gold',
+  'lime',
+  'green',
+  'cyan',
+  'purple'
+]
