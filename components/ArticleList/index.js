eeccc3fee76afc752bfb14d132954a40030131ff
leofe <leofe200@163.com>
2019-12-01 21:45:28 +0800

    :sparkles: update

--- components/ArticleList/index.js
+++ components/ArticleList/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Divider } from 'antd'
+import { Divider, Tag, Icon } from 'antd'
 
 const ArticleList = () => {
   return (
     <ul className="article-list">
       <li className="article">
-        <p>实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM</p>
+        <Divider orientation="left">
+          <span className="article-title">javascript 基础系列1 - 原型与原型链</span>
+          <span className="post-time">2019-11-10</span>
+        </Divider>
+        <div className="article-content">实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM实现简易的 VUE-MVVM</div>
+        <div className="article-info">
+          <Icon type="message" />
+          <span className="comment-count">5</span>
+          <Icon type="eye" />
+          <span className="view-count">10</span>
+          <Tag>javascript</Tag>
+        </div>
       </li>
     </ul>
   )
